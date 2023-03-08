2521c527aa6d59f4191c7dcbd033a2287554c272
pointlessrapunzel <savivalto@gmail.com>
2023-03-08 21:31:21 +0500

    (fix): help section icons

--- src/components/HelpSection.tsx
+++ src/components/HelpSection.tsx
@@ -13,8 +13,10 @@ export default function HelpSection(props: { helpData: HelpData[] }) {
       <div className='col-contain grid grid-cols-4 place-content-center place-items-center gap-x-32 gap-y-24'>
         {props.helpData.map((el) => (
           <Link key={el.shortTitle} href={`/help/${el.slug}`}>
-            <Card className='flex h-[312px] w-[302px] flex-col items-center justify-center gap-8 text-center text-2xl font-extrabold uppercase'>
-              <Image src={el.linkImage} width={122} height={122} alt='' />
+            <Card className='flex h-[312px] w-[302px] flex-col items-center justify-between gap-8 text-center text-2xl font-extrabold uppercase'>
+              <div className='flex grow items-center'>
+                <Image src={el.linkImage} width={130} height={130} alt='' />
+              </div>
               <p>{el.shortTitle}</p>
             </Card>
           </Link>
