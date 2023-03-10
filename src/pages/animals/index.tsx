e9e355b5be4f747393803c76e9bb4a4d705a5fa1
pointlessrapunzel <savivalto@gmail.com>
2023-03-10 20:32:54 +0500

    (fix): Focus state on filter chips on animals page

--- src/pages/animals/index.tsx
+++ src/pages/animals/index.tsx
@@ -81,7 +81,7 @@ function Filter() {
                   />
                   <label
                     htmlFor={`filter-${o}`}
-                    className='cursor-pointer select-none rounded border border-black bg-white px-6 py-0.5 font-light outline-offset-2 peer-checked:bg-brand-300 peer-focus:outline peer-focus:outline-2 peer-focus:outline-offset-0 peer-focus:outline-highlight'
+                    className='cursor-pointer select-none rounded border border-black bg-white px-6 py-0.5 font-light outline-offset-2 peer-checked:bg-brand-300 peer-focus-visible:outline peer-focus-visible:outline-2 peer-focus-visible:outline-offset-0 peer-focus-visible:outline-highlight'
                   >
                     {o}
                   </label>
