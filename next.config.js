6ebfad1691605a413fcca1378428d1d0fc649ed3
pointlessrapunzel <savivalto@gmail.com>
2023-04-01 15:05:14 +0500

    Add script for static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const { PHASE_EXPORT } = require('next/constants')
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_EXPORT) {
+    return {
+      reactStrictMode: true,
+      images: {
+        unoptimized: true,
+      },
+    }
+  }
+
+  return {
+    reactStrictMode: true,
+  }
+}
